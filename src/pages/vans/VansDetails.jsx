756a05f4e0a8b7fcd7f1fced916f1c2b40fc089f
ivad <yvesyvad@gmail.com>
2023-05-22 15:52:11 +0200

    Finalize search params section

--- src/pages/vans/VansDetails.jsx
+++ src/pages/vans/VansDetails.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import VanButton from "../../components/VanButton";
 import CenterContent from "../../components/layout/CenterContent";
 
 const VansDetails = () => {
+  const location = useLocation();
   const { id } = useParams();
   const [van, setVan] = useState({});
+
   const { imageUrl, name, price, type, description } = van;
+  const selectedType = location.state?.type || "all";
 
   useEffect(() => {
     const fetchVans = async () => {
@@ -24,23 +27,32 @@ const VansDetails = () => {
         {Object.keys(van).length === 0 ? (
           <h2 className="mt-5 text-2xl font-semibold">Loading...</h2>
         ) : (
-          <div className="flex flex-col sm:flex-row items-end gap-10">
-            <img
-              src={imageUrl}
-              alt={name}
-              className="flex-shrink-0 hidden sm:block rounded object-cover sm:w-1/2 relative h-[400px]"
-            />
-            <div className="mt-10 sm:mt-0">
-              <VanButton>{type}</VanButton>
-              <h1 className="mt-4 text-2xl font-bold">{name}</h1>
-              <p className="mt-2 text-xl font-semibold">
-                ${price}
-                <span className="text-lg font-medium">/day</span>
-              </p>
-              <p className="mt-4 text-lg">{description}</p>
-              <button className="w-full py-2 mt-10 font-semibold rounded lg:py-3 text-light bg-orange-primary hover:bg-orange-700">
-                Rent your van
-              </button>
+          <div>
+            <Link to={`..?${location.state?.searchParams || ""}`} relative="path">
+              {/* &lt;-- to simulate an arrow */}
+              &lt;--{" "}
+              <span className="font-semibold underline cursor-pointer">
+                Back to {selectedType} vans
+              </span>
+            </Link>
+            <div className="flex flex-col sm:flex-row items-end gap-10 mt-5">
+              <img
+                src={imageUrl}
+                alt={name}
+                className="flex-shrink-0 hidden sm:block rounded object-cover sm:w-1/2 relative h-[400px]"
+              />
+              <div className="mt-10 sm:mt-0">
+                <VanButton>{type}</VanButton>
+                <h1 className="mt-4 text-2xl font-bold">{name}</h1>
+                <p className="mt-2 text-xl font-semibold">
+                  ${price}
+                  <span className="text-lg font-medium">/day</span>
+                </p>
+                <p className="mt-4 text-lg">{description}</p>
+                <button className="w-full py-2 mt-10 font-semibold rounded lg:py-3 text-light bg-orange-primary hover:bg-orange-700">
+                  Rent your van
+                </button>
+              </div>
             </div>
           </div>
         )}
